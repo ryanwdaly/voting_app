94b223b6046c4eb043f1057b319db8bec08a0a8f
ryan.w.daly <ryan.w.daly90@gmail.com>
2020-04-23 21:10:17 -0400

    done for the night

--- public/js/app-1.js
+++ public/js/app-1.js
@@ -2,7 +2,7 @@ class ProductList extends React.Component {
   render() {
     return (
       <div className='ui unstackable items'>
-        Hello, friend! I am a basic React component.
+          Hello, friend! I am a basic React component.
       </div>
     );
   }
